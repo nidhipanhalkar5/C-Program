e365e9c7835ec0777bc4d8cbf87dabf21e2acb6c
nidhipanhalkar5 <nidhipanhalkar5@gmail.com>
2025-11-04 18:16:36 +0530

    Add files via upload
    to cheak whether a number is negative , positive or zero

---
+++ number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    if(num>0)
+    {
+        printf("The number is positive \n");
+    }
+    else if(num<0)
+    {
+        printf("The number is negative \n");
+    }
+    else{
+        printf("The numbe is zero \n");
+    }
+    return 0;
+}
