297d66fd26bccc0f243cdd691c82bc9cc26e878c
nidhipanhalkar5 <nidhipanhalkar5@gmail.com>
2025-11-04 09:10:33 +0530

    Add files via upload
    Addition

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	float a, b, ans;
+
+	printf("Enter the first number :");
+	scanf("%f",&a);
+
+	printf("Enter the second number :");
+	scanf("%f",&b);
+
+	ans= a+b;
+
+	printf("The sum is: %3f \n" ,ans);
+	return 0;
+}
