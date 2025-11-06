207e7f8936e2a82be37642d7ac79a4e9cf7ebff5
nidhipanhalkar5 <nidhipanhalkar5@gmail.com>
2025-11-06 23:29:21 +0530

    Add files via upload
    print n natural number from  1 to n

---
+++ n-natural.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+
+    printf("How many times you want print:");
+    scanf("%d",&n);
+
+        printf("Natural Numbers :");
+
+
+    for(i=1;i<=n;i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
