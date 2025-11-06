2391281bc3b5ba0c229b16f3319ba1300f7465d3
nidhipanhalkar5 <nidhipanhalkar5@gmail.com>
2025-11-06 23:21:28 +0530

    Add files via upload
    display the massage n-times

---
+++ display.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+
+    printf("How many times you want to print :");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("Welcome to ICEM !!!! \n");
+    }
+    return 0;
+}
